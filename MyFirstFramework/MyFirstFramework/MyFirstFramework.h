39772b2487113ded9c4a0978dfef952390f3ff53
Sridatta <sridatta@krify.net>
2023-10-13 12:46:32 +0530

    Initial framework setup done

---
+++ MyFirstFramework/MyFirstFramework/MyFirstFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyFirstFramework.h
+//  MyFirstFramework
+//
+//  Created by KSMACMINI-017 on 13/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstFramework.
+FOUNDATION_EXPORT double MyFirstFrameworkVersionNumber;
+
+//! Project version string for MyFirstFramework.
+FOUNDATION_EXPORT const unsigned char MyFirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstFramework/PublicHeader.h>
+
+
